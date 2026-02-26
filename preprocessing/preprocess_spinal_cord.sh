c1be10c3cb59bebd67a1dd4cfd7bfa68c5cd8867
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-02-26 14:23:42 -0800

    update dilation to ensure all slices are coverded for moco

--- preprocessing/preprocess_spinal_cord.sh
+++ preprocessing/preprocess_spinal_cord.sh
@@ -243,7 +243,7 @@ if [[ $SES == *"spinalcord"* ]];then
 
   
     runs=(rest rightthumb leftthumb rightmiddle leftmiddle rightpinky leftpinky)
-    runs=(rest)
+    #runs=(rest)
     for run in "${runs[@]}";do
 
       cd ${PATH_DATA_PROCESSED}/${SUBJECT}/func/
@@ -292,7 +292,7 @@ if [[ $SES == *"spinalcord"* ]];then
             # Segment the spinal cord
             segment_if_does_not_exist ${file_task_mean} 't2s' 'epi' 'func'
             # Dilate the spinal cord mask
-            ${SCT_EXEC}sct_maths -i ${file_task_mean}_label-SC_seg.nii.gz -dilate 8 -shape disk -o ${file_task_mean}_mask.nii.gz -dim 2
+            ${SCT_EXEC}sct_maths -i ${file_task_mean}_label-SC_seg.nii.gz -dilate 8x8x12 -shape ball -o ${file_task_mean}_mask.nii.gz
           fi
           # Qc of mask
           ${SCT_EXEC}sct_qc -i ${file_task_mean}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -s ${file_task_mean}_mask.nii.gz -qc-subject ${SUBJECT}
@@ -321,7 +321,7 @@ if [[ $SES == *"spinalcord"* ]];then
             # Segment the spinal cord
               segment_if_does_not_exist mc1_mean 't2s' 'epi' 'func'
               # check dilating
-              ${SCT_EXEC}sct_maths -i mc1_mean_label-SC_seg.nii.gz -dilate 8 -shape disk -o mc1_mask.nii.gz -dim 2
+              ${SCT_EXEC}sct_maths -i mc1_mean_label-SC_seg.nii.gz -dilate 8x8x12 -shape ball -o mc1_mask.nii.gz
               # Qc of mask
               ${SCT_EXEC}sct_qc -i  mc1_mean.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -s mc1_mask.nii.gz -qc-subject ${SUBJECT}
             fi
