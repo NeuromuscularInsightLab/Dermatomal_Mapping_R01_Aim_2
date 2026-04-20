0593d588fe8705d06c7abf1067190c36885398e7
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-04-20 14:12:28 -0700

    fix whole spine acc and add axial

--- preprocessing/convert_dcm2bids.sh
+++ preprocessing/convert_dcm2bids.sh
@@ -176,7 +176,7 @@ for file in $(ls -1v -- *.json); do
     cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_T2w.json
     cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_T2w.nii.gz
 
-  elif [[ ${series} == *"T2w_whole-spine"* ]] && [[ ${series} == *"cs25"* ]]; then
+  elif [[ ${series} == *"T2w_whole-spine"* ]] && [[ ${series} == *"cs25"* ]] && [[ ${series} != *"COMP"* ]]; then
     echo ${series}
         protocol=`grep 'ProtocolName' ${filename}.json`
     echo ${protocol}
@@ -194,19 +194,24 @@ for file in $(ls -1v -- *.json); do
     cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_rec-accel_T2w.json
     cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_rec-accel_T2w.nii.gz
   
-  elif [[ ${series} == *"T2w_whole-spine"* ]] && [[ ${series} == *"cs25"* ]]&& [[ ${series} == *"COMP"* ]]; then
+  elif [[ ${series} == *"T2w_whole-spine"* ]] && [[ ${series} == *"cs25"* ]] && [[ ${series} == *"COMP"* ]]; then
     acq=wholespine
-    cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_T2w.json
-    cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_T2w.nii.gz
+    echo "here"
+    cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_rec-accel_T2w.json
+    cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-${acq}_rec-accel_T2w.nii.gz
 
   elif [[ ${series} == *"T2w_c-spine"* ]]; then
     cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-cervical_T2w.json
     cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-cervical_T2w.nii.gz
   
-  elif [[ ${series} == *"T2w_clinical"* ]]; then
+  elif [[ ${series} == *"T2w_clinical_c-spine_sag"* ]]; then
     cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-sag_T2w.json
     cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-sag_T2w.nii.gz
   
+  elif [[ ${series} == *"T2w_clinical_c-spine_ax"* ]]; then
+    cp ${filename}.json ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-ax_T2w.json
+    cp ${filename}.nii.gz ${output_path}/ses-spinalcord${session}/anat/${subject}_ses-spinalcord${session}_acq-ax_T2w.nii.gz
+
   ###########################################################################################
   ###########################################################################################
   #Brachial Plexus STIR
