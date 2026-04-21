0a2077c5b1351c9f26e9827c903cd47c61c5db74
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-04-21 15:00:06 -0700

    add option to exclude pul or resp

--- preprocessing/create_FSL_physio_text_file_from_Siemens_file.py
+++ preprocessing/create_FSL_physio_text_file_from_Siemens_file.py
@@ -38,6 +38,8 @@ def get_parser():
                         help="Number of volumes collected")
     parser.add_argument('-exclude-resp', action='store_true',
                         help="To put 0 values in respiratory data")
+    parser.add_argument('-exclude-pulse', action='store_true',
+                        help="To put 0 values in pulse data")
     return parser
 
 def plot_data(cardiac_time_data, cardiac_data, respiration_data_interp, trigger_data):
@@ -76,7 +78,7 @@ def plot_data(cardiac_time_data, cardiac_data, respiration_data_interp, trigger_
     plt.savefig('physio.png', dpi=600, bbox_inches="tight")
 
 
-def create_FSL_physio_text_file_from_Siemens_file(pulse_fname, resp_fname, json_fname, TR, number_of_volumes):
+def create_FSL_physio_text_file_from_Siemens_file(pulse_fname, resp_fname, json_fname, TR, number_of_volumes, exclude_resp=False, exclude_pulse=False):
     """
     Converts Siemens physiological log files to FSL-compatible .physio text file.
     Sampling rate = 50 Hz
@@ -84,128 +86,130 @@ def create_FSL_physio_text_file_from_Siemens_file(pulse_fname, resp_fname, json_
     """
 
     # ------------------ Read Pulse data ------------------
-    with open(pulse_fname, 'r') as f:
-        pulse_data = f.read()
-    f = None
-    try:
-        f = pulse_fname if hasattr(pulse_fname, 'read') else open(pulse_fname)
-        lines = [line for line in f]
-    finally:
-        if f:
-            f.close()
-    fields = lines[0].split(' ')
-    print(fields[:40])
-    # find first occurrence of '6002' element in fields
-    first_6002_index = fields.index('6002') if '6002' in fields else -1
-    print(first_6002_index)
+    if not exclude_pulse:
+        with open(pulse_fname, 'r') as f:
+            pulse_data = f.read()
+        f = None
+        try:
+            f = pulse_fname if hasattr(pulse_fname, 'read') else open(pulse_fname)
+            lines = [line for line in f]
+        finally:
+            if f:
+                f.close()
+        fields = lines[0].split(' ')
+        print(fields[:40])
+        # find first occurrence of '6002' element in fields
+        first_6002_index = fields.index('6002') if '6002' in fields else -1
+        print(first_6002_index)
 
-    pulse_trace = np.array(fields[first_6002_index + 1 :])[:-1]#.astype(float)  # remove last 5003 trigger
+        pulse_trace = np.array(fields[first_6002_index + 1 :])[:-1]#.astype(float)  # remove last 5003 trigger
 
-    #pulse_indices = [m.start() for m in re.finditer('6002', first_line)]
-   # pulse_trace = np.fromstring(first_line[pulse_indices[0]:], sep=' ', dtype=int)
+        #pulse_indices = [m.start() for m in re.finditer('6002', first_line)]
+    # pulse_trace = np.fromstring(first_line[pulse_indices[0]:], sep=' ', dtype=int)
 
 
-    pulse_start_time = int(re.search(r'LogStartMDHTime:\s+(\d+)', pulse_data).group(1))
-    pulse_stop_time = int(re.search(r'LogStopMDHTime:\s+(\d+)', pulse_data).group(1))
+        pulse_start_time = int(re.search(r'LogStartMDHTime:\s+(\d+)', pulse_data).group(1))
+        pulse_stop_time = int(re.search(r'LogStopMDHTime:\s+(\d+)', pulse_data).group(1))
 
-    # Remove peak detection spikes (value == 5000)
-    clean_fields = []
-    include = True
-    ninfos = 0
-    debug = True
-    for field in pulse_trace:  # TODO: why 8?
-        if include:
-            if field == '5002':
-                include = False
-                excluded = []
+        # Remove peak detection spikes (value == 5000)
+        clean_fields = []
+        include = True
+        ninfos = 0
+        debug = True
+        for field in pulse_trace:  # TODO: why 8?
+            if include:
+                if field == '5002':
+                    include = False
+                    excluded = []
+                else:
+                    clean_fields.append(field)
             else:
-                clean_fields.append(field)
-        else:
-            if field == '6002':
-                include = True
-                if debug:
-                    print("Excluded info field: '%s'" % ' '.join(excluded))
-                    ninfos += 1
-            else:
-                excluded.append(field)
+                if field == '6002':
+                    include = True
+                    if debug:
+                        print("Excluded info field: '%s'" % ' '.join(excluded))
+                        ninfos += 1
+                else:
+                    excluded.append(field)
 
-    pulse_trace = np.asarray([int(field) for field in clean_fields])
-    spikes = np.where(pulse_trace == 5000)[0]
-    for i in spikes:
-        if 0 < i < len(pulse_trace) - 1:
-            pulse_trace[i] = (pulse_trace[i - 1] + pulse_trace[i + 1]) / 2
+        pulse_trace = np.asarray([int(field) for field in clean_fields])
+        spikes = np.where(pulse_trace == 5000)[0]
+        for i in spikes:
+            if 0 < i < len(pulse_trace) - 1:
+                pulse_trace[i] = (pulse_trace[i - 1] + pulse_trace[i + 1]) / 2
 
-    # Round start and stop times to nearest 20 ms (5 ms in ms units = 20 ms step)
-    pulse_start_time = round(pulse_start_time / 20) * 20
-    pulse_stop_time = round(pulse_stop_time / 20) * 20
+        # Round start and stop times to nearest 20 ms (5 ms in ms units = 20 ms step)
+        pulse_start_time = round(pulse_start_time / 20) * 20
+        pulse_stop_time = round(pulse_stop_time / 20) * 20
 
-    # Create pulse time vector and resample to 50 Hz (20 ms)
-    pulse_time = np.arange(pulse_start_time, pulse_stop_time + 1, 20)
-    print('length pulse time:', len(pulse_time), 'length pulse trace:', len(pulse_trace))
-    pulse_trace = decimate(pulse_trace, 8)
-    figure, ax = plt.subplots()
-    ax.plot(pulse_trace)
-    plt.savefig('pulse_trace_plot.png', dpi=300, bbox_inches="tight")
-    plt.close(figure)
+        # Create pulse time vector and resample to 50 Hz (20 ms)
+        pulse_time = np.arange(pulse_start_time, pulse_stop_time + 1, 20)
+        print('length pulse time:', len(pulse_time), 'length pulse trace:', len(pulse_trace))
+        pulse_trace = decimate(pulse_trace, 8)
+        figure, ax = plt.subplots()
+        ax.plot(pulse_trace)
+        plt.savefig('pulse_trace_plot.png', dpi=300, bbox_inches="tight")
+        plt.close(figure)
     # ------------------ Read Resp data ------------------
-    with open(resp_fname, 'r') as f:
-         resp_data = f.read()
-    try:
-        f = resp_fname if hasattr(resp_fname, 'read') else open(resp_fname)
-        lines = [line for line in f]
-    finally:
-        if f:
-            f.close()
-    fields = lines[0].split(' ')
-    print(fields[:40])
-    # find 5th occurrence of '6002' element in fields
-    # Find the 5th occurrence of '6002' in fields
-    occurrences = [i for i, x in enumerate(fields) if x == '6002']
-    fifth_6002_index = occurrences[4] if len(occurrences) >= 5 else -1
-    resp_trace = np.array(fields[fifth_6002_index + 1 :])[:-1]  # remove last 5003 trigger
+    if not exclude_resp:
+        with open(resp_fname, 'r') as f:
+            resp_data = f.read()
+        try:
+            f = resp_fname if hasattr(resp_fname, 'read') else open(resp_fname)
+            lines = [line for line in f]
+        finally:
+            if f:
+                f.close()
+        fields = lines[0].split(' ')
+        print(fields[:40])
+        # find 5th occurrence of '6002' element in fields
+        # Find the 5th occurrence of '6002' in fields
+        occurrences = [i for i, x in enumerate(fields) if x == '6002']
+        fifth_6002_index = occurrences[4] if len(occurrences) >= 5 else -1
+        resp_trace = np.array(fields[fifth_6002_index + 1 :])[:-1]  # remove last 5003 trigger
 
-    resp_start_time = int(re.search(r'LogStartMDHTime:\s+(\d+)', resp_data).group(1))
-    resp_stop_time = int(re.search(r'LogStopMDHTime:\s+(\d+)', resp_data).group(1))
-    clean_fields = []
-    include = True
-    ninfos = 0
-    debug = True
-    for field in resp_trace:  # TODO: why 8?
-        if include:
-            if field == '5002':
-                include = False
-                excluded = []
+        resp_start_time = int(re.search(r'LogStartMDHTime:\s+(\d+)', resp_data).group(1))
+        resp_stop_time = int(re.search(r'LogStopMDHTime:\s+(\d+)', resp_data).group(1))
+        clean_fields = []
+        include = True
+        ninfos = 0
+        debug = True
+        for field in resp_trace:  # TODO: why 8?
+            if include:
+                if field == '5002':
+                    include = False
+                    excluded = []
+                else:
+                    clean_fields.append(field)
             else:
-                clean_fields.append(field)
-        else:
-            if field == '6002':
-                include = True
-                if debug:
-                    #print("Excluded info field: '%s'" % ' '.join(excluded))
-                    ninfos += 1
-            else:
-                excluded.append(field)
+                if field == '6002':
+                    include = True
+                    if debug:
+                        #print("Excluded info field: '%s'" % ' '.join(excluded))
+                        ninfos += 1
+                else:
+                    excluded.append(field)
 
-    resp_trace = np.asarray([int(field) for field in clean_fields])
+        resp_trace = np.asarray([int(field) for field in clean_fields])
 
-    resp_trace = resp_trace.astype(float)
-    spikes = np.where(resp_trace == 5000)[0]
-    for i in spikes:
-        if 0 < i < len(resp_trace) - 1:
-            resp_trace[i] = (resp_trace[i - 1] + resp_trace[i + 1]) / 2
+        resp_trace = resp_trace.astype(float)
+        spikes = np.where(resp_trace == 5000)[0]
+        for i in spikes:
+            if 0 < i < len(resp_trace) - 1:
+                resp_trace[i] = (resp_trace[i - 1] + resp_trace[i + 1]) / 2
 
-    # Round start and stop times to nearest 20 ms
-    resp_start_time = round(resp_start_time / 20) * 20
-    resp_stop_time = round(resp_stop_time / 20) * 20
+        # Round start and stop times to nearest 20 ms
+        resp_start_time = round(resp_start_time / 20) * 20
+        resp_stop_time = round(resp_stop_time / 20) * 20
 
-    # Create resp time vector and resample to 50 Hz (20 ms)
-    resp_time = np.arange(resp_start_time, resp_stop_time + 1, 20)
-    resp_trace = decimate(resp_trace, 8)
+        # Create resp time vector and resample to 50 Hz (20 ms)
+        resp_time = np.arange(resp_start_time, resp_stop_time + 1, 20)
+        resp_trace = decimate(resp_trace, 8)
 
-    figure, ax = plt.subplots()
-    ax.plot(resp_trace)
-    plt.show()
-    plt.close(figure)
+        figure, ax = plt.subplots()
+        ax.plot(resp_trace)
+        plt.show()
+        plt.close(figure)
     # ------------------ Read JSON data ------------------
     with open(json_fname, 'r') as f:
         json_data = json.load(f)
@@ -234,14 +238,19 @@ def create_FSL_physio_text_file_from_Siemens_file(pulse_fname, resp_fname, json_
         if start_idx.size == 0 or stop_idx.size == 0:
             raise ValueError("Time indices not found in physiological data range.")
         return trace[start_idx[0]:stop_idx[0] + 1]
-
-    pulse_vector = get_segment(pulse_trace, pulse_time,
-                               acquisition_time - (TR * 1000),
-                               acquisition_time + (TR * number_of_volumes * 1000))
+    if not exclude_pulse:
+        pulse_vector = get_segment(pulse_trace, pulse_time,
+                                acquisition_time - (TR * 1000),
+                                acquisition_time + (TR * number_of_volumes * 1000))
+    else:
+        pulse_vector = np.zeros_like(time_vector)
     #print('Pulse vector', pulse_vector)
-    resp_vector = get_segment(resp_trace, resp_time,
-                              acquisition_time - (TR * 1000),
-                              acquisition_time + (TR * number_of_volumes * 1000))
+    if not exclude_resp:
+        resp_vector = get_segment(resp_trace, resp_time,
+                                acquisition_time - (TR * 1000),
+                                acquisition_time + (TR * number_of_volumes * 1000))
+    else:
+        resp_vector = np.zeros_like(time_vector)
     #print('Resp vector', resp_vector)
     # ------------------ Create trigger vector ------------------
     trigger_starts = np.arange(acquisition_time,
@@ -297,12 +306,16 @@ def main():
     json_fname = args.json
     resp_fname = args.resp
     pulse_fname = args.pulse
+    exclude_resp = args.exclude_resp
+    exclude_pulse = args.exclude_pulse
 
     time_vector, pulse_vector, trigger_vector, resp_vector = create_FSL_physio_text_file_from_Siemens_file(pulse_fname=pulse_fname,
                                                                                                            resp_fname=resp_fname,
                                                                                                            json_fname=json_fname,
                                                                                                            TR=TR, 
-                                                                                                           number_of_volumes=number_of_volumes)
+                                                                                                           number_of_volumes=number_of_volumes,
+                                                                                                           exclude_resp=args.exclude_resp,
+                                                                                                           exclude_pulse=args.exclude_pulse)  
 
 if __name__ == '__main__':
     main()
