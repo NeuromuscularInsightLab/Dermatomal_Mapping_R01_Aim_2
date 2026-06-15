8fb33d86d320451fb22628b6e75ef6e1e29a1cb6
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-06-15 11:27:25 -0700

    update to run rest only

--- preprocessing/preprocess.sh
+++ preprocessing/preprocess.sh
@@ -4,10 +4,10 @@
 path_script=~/codes/Dermatomal_Mapping_R01_Aim_2/preprocessing
 PATH_DATA=${SCRATCH}/dm-aim2/sourcedata
 PATH_SEGMANUAL=${SCRATCH}/dm-aim2/derivatives/labels
-output_path=${SCRATCH}/dm-aim2/preprocessing_ALL_nordic_v2
+output_path=${SCRATCH}/dm-aim2/preprocessing_ALL_rest
 subjects=(sub-DMAim2HC001 sub-DMAim2HC002 sub-DMAim2HC003 sub-DMAim2HC004 sub-DMAim2HC013 sub-DMAim2HC009 sub-DMAim2HC012 sub-DMAim2HC020 sub-DMAim2HC011 sub-DMAim2HC005 sub-DMAim2HC016 sub-DMAim2HC021 sub-DMAim2HC018 sub-DMAim2HC023 sub-DMAim2HC014 sub-DMAim2HC025 sub-DMAim2HC017 sub-DMAim2HC019 sub-DMAim2HC027 sub-DMAim2HC026 sub-DMAim2HC006)
 ses=ses-spinalcord
-NORDIC="nordic" #nordic
+NORDIC="" #nordic
 time_limit=17:00:00
 memory=64000
 
